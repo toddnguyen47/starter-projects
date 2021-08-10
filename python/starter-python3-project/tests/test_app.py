993ff3684f4ec145be90d3b930dc6fd4156fddc8
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-09 18:36:51 -0700

    Finally got pre-hooks to work!

--- python/starter-python3-project/tests/test_app.py
+++ python/starter-python3-project/tests/test_app.py
@@ -2,16 +2,16 @@
 
 import pytest
 
-import src.app
+from src import app
 
 
 @pytest.fixture(scope="class")
 def _init_app():
     """Return a fixture that initializes the App() object"""
-    app_obj = src.app.App()
+    app_obj = app.App()
     yield app_obj
 
 
-def test_given_two_when_getting_two_then_should_be_two(_init_app: src.app):
+def test_given_two_when_getting_two_then_should_be_two(_init_app: app):
     """Should return two"""
     assert _init_app.return_two() == 2
