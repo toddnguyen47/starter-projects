8028157da7bb2c459be4a2baf1166fb8eab0f006
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-12 12:03:38 -0700

    Add pre-commit hooks

--- java/starter-maven/run-pmd-analysis.sh
+++ java/starter-maven/run-pmd-analysis.sh
@@ -3,6 +3,19 @@
 set -eu -o pipefail
 
 _pmd_analysis_dir="pmd-analysis"
-pmd -d src\
-  -cache "${_pmd_analysis_dir}/pmd-cache.bin" \
-  -rulesets "${_pmd_analysis_dir}/pmd-ruleset.xml"
+_prefix="java/starter-maven"
+
+echo hi > output.log
+printf "$@\n" >> output.log
+
+for vararg in "$@"; do
+  # Ref: https://stackoverflow.com/a/16623897/6323360
+  # Need to delete the prefix
+  _cur_file="${vararg#${_prefix}}"
+  echo "${_cur_file}" >> output.log
+  pmd -d "${_cur_file}"\
+    -cache "${_pmd_analysis_dir}/pmd-cache.bin" \
+    -rulesets "${_pmd_analysis_dir}/pmd-ruleset.xml"
+done
+
+exit 4
