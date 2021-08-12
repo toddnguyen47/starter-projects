95856ea056cf928da27348d9b4199dec227416ac
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-12 10:28:56 -0700

    Add logger for java maven

--- java/starter-maven/src/main/java/com/github/toddnguyen47/Application.java
+++ java/starter-maven/src/main/java/com/github/toddnguyen47/Application.java
@@ -1,8 +1,20 @@
 package com.github.toddnguyen47;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Remember to change your project name!");
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
+    public static void main(final String[] args) {
+        LOGGER.info("Remember to change your project name!");
+
+        LOGGER.fatal("fatal");
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
     }
 
     public int returnTwo() {
--- java/starter-maven/src/test/java/com/github/toddnguyen47/ApplicationTest.java
+++ java/starter-maven/src/test/java/com/github/toddnguyen47/ApplicationTest.java
@@ -3,10 +3,10 @@ package com.github.toddnguyen47;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationTest {
+class ApplicationTest {
     @Test
-    public void TestTwo() {
-        Application application = new Application();
+    void testTwo() {
+        final Application application = new Application();
 
         Assertions.assertEquals(2, application.returnTwo());
     }
