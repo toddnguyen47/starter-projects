a4211630031082c360b8e04cb13d0064066350c6
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-12 15:30:58 -0700

    Update run_pmd.py for Java

--- java/starter-gradle/pre_commit_hooks/run_pmd.py
+++ java/starter-gradle/pre_commit_hooks/run_pmd.py
@@ -6,16 +6,14 @@ from typing import Optional, Sequence
 
 # Ref: https://github.com/pre-commit/pre-commit-hooks/blob/master/pre_commit_hooks/trailing_whitespace_fixer.py
 _PMD_ANALYSIS_FOLDER = "java/starter-gradle/pmd-analysis"
-_INDEX_DIR = 2
 
 _pmd_cmd = [
     "pmd",
-    "-dir",
-    "",
     "-cache",
     '"' + _PMD_ANALYSIS_FOLDER + '/pmd-cache.bin"',
     "-rulesets",
     '"' + _PMD_ANALYSIS_FOLDER + '/pmd-ruleset.xml"',
+    "-dir"
 ]
 
 
@@ -27,15 +25,14 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     return_code = 0
     for filename in args.filenames:
-        _pmd_cmd[_INDEX_DIR] = filename
-        process = subprocess.Popen(_pmd_cmd)
-        try:
-            _outs, _errs = process.communicate(timeout=15)
-        except subprocess.TimeoutExpired:
-            process.kill()
-            _outs, _errs = process.communicate()
-        return_code += process.returncode
-        process.terminate()
+        _pmd_cmd.append(filename)
+        with subprocess.Popen(_pmd_cmd) as process:
+            try:
+                _outs, _errs = process.communicate(timeout=15)
+            except subprocess.TimeoutExpired:
+                process.kill()
+                _outs, _errs = process.communicate()
+            return_code += process.returncode
     return return_code
 
 
