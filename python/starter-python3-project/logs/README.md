677b53ae1aea2d97eca6f1d45e86ae67e72566b2
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-25 09:58:33 -0700

    Add logging to python starter project

---
+++ python/starter-python3-project/logs/README.md
@@ -0,0 +1,3 @@
+# logs
+
+Logs folder to keep all the logs. Note that all logs will be ignored by `.gitignore` on default to not pollute the repository.
--- python/starter-python3-project/src/app.py
+++ python/starter-python3-project/src/app.py
@@ -1,5 +1,7 @@
 """Default app"""
-from src.mymath import mymath
+from mymath import mymath
+import logger_color
+from logger_color import LoggerType
 
 
 class App:
@@ -12,3 +14,17 @@ class App:
         """Returns the number 2"""
         self._variable_x = mymath.return_two()
         return self._variable_x
+
+
+def _main():
+    """Main function to run binary"""
+    logger = logger_color.init_logger(__name__, LoggerType.BOTH)
+    logger.debug("DEBUG")
+    logger.info("INFO")
+    logger.warning("WARNING")
+    logger.error("ERROR")
+    logger.critical("CRITICAL, cannot continue")
+
+
+if __name__ == "__main__":
+    _main()
