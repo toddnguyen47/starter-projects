2c0016dcccf7c73f0d855290ab7d629ba1cf8d17
Todd Nguyen <todd.nguyen47@gmail.com>
2021-08-30 16:52:19 -0700

    Add `>>>` to beginning of Python logs

--- python/starter-python3-project/src/logger_color.py
+++ python/starter-python3-project/src/logger_color.py
@@ -13,7 +13,7 @@ from colorama import Fore, Style
 colorama.init()
 
 _LOGS_FOLDER = "logs"
-_LOG_FORMAT_STR_STREAM = "[%(asctime)s] [%(levelname)-10s] %(message)s " + "[%(filename)s:%(lineno)d]"
+_LOG_FORMAT_STR_STREAM = "[%(asctime)s] [%(levelname)-10s] >>> %(message)s " + "[%(filename)s:%(lineno)d]"
 _LOG_FORMAT_STR_FILE = _LOG_FORMAT_STR_STREAM + " [Logger Name: %(name)s]"
 _DATE_FMT_STR = "%Y-%m-%dT%H:%M:%S"
 
