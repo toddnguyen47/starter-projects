0a910c3cb6aee08a71cb262188160417250edfb9
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-10-11 10:39:25 -0700

    Update rollup.config.js

--- javascript/rollup.config.js
+++ javascript/rollup.config.js
@@ -2,6 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 
+const skippedWarnings = new Set([
+  'THIS_IS_UNDEFINED',
+]);
+
 // Ref: https://rollupjs.org/guide/en/#babel
 
 export default {
@@ -19,4 +23,18 @@ export default {
     resolve(),
     babel({babelHelpers: 'bundled'}),
   ],
+  /**
+   * Ref: https://github.com/rollup/rollup/issues/1518#issuecomment-321875784
+   * @param {Object} warning
+   * @param {Function} warn
+   * @returns
+   */
+  onwarn(warning, warn) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if (skippedWarnings.has(warning.code)) { return; }
+
+    warn(warning);
+  },
 };
