fc47b1848bb95beda5c780a8fd4d2a6ee88f5408
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-11-11 16:03:28 -0800

    Update run_pmd.py

--- java/pre-commit/pre_commit_hooks/run_pmd.py
+++ java/pre-commit/pre_commit_hooks/run_pmd.py
@@ -8,11 +8,8 @@ import os
 import time
 import sys
 
-import dominate
-from dominate import tags
-
 # Ref: https://github.com/pre-commit/pre-commit-hooks/blob/master/pre_commit_hooks/trailing_whitespace_fixer.py
-_PREFIX = "change/path/to/"
+_PREFIX = "change/path/to"
 _PMD_ANALYSIS_FOLDER = "pmd-analysis"
 _UTF8_ENCODING = "utf-8"
 _CACHE_FILE_EXT = ".bin"
@@ -20,8 +17,8 @@ _BASE_CACHE_NAME_MAIN = "pmd-cache-main" + _CACHE_FILE_EXT
 _BASE_CACHE_NAME_TEST = "pmd-cache-test" + _CACHE_FILE_EXT
 _INDEX_CACHE_NAME = 2
 _OUTPUT_FORMAT = "text"
-_OUTOUT_MAIN_FILE = "/output-main.html"
-_OUTPUT_TEST_FILE = "/output-test.html"
+_OUTOUT_MAIN_FILE = "/output-main.txt"
+_OUTPUT_TEST_FILE = "/output-test.txt"
 
 _pmd_cmd_main = collections.deque(
     [
@@ -84,8 +81,8 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             return_code = max(return_code, process.returncode)
             with open(output_file, "a+", encoding=_UTF8_ENCODING) as file:
                 str_to_write = stdout.decode(_UTF8_ENCODING).strip()
-                html_text = _generate_html_from_txt(output_file, str_to_write)
-                file.write(str(html_text))
+                file.write(str_to_write)
+                file.write("\n")
         cmd.pop()
     return return_code
 
@@ -96,27 +93,6 @@ def _remove_output_files(filenames: List[str]):
         if os.path.exists(filename):
             os.remove(filename)
             time.sleep(0.5)
-
-
-def _generate_html_from_txt(doc_title: str, input_str: str) -> str:
-    """Generate a simple HTML file from txt file"""
-    doc = dominate.document(title=doc_title)
-
-    with doc.head:
-        tags.style(
-            r"html{font-size:62.5%;}body{font-size:1.8rem;}div.content{width:1024px;margin:auto;}"
-            + r"p.pmd-text{font-family:monospace;}"
-        )
-
-    with doc:
-        with tags.div(_class="content"):
-            if input_str.strip():
-                for line in input_str.split("\n"):
-                    tags.p(line, _class="pmd-text")
-            else:
-                tags.p("You're good! No errors ✨ 🍰 ✨", _class="pmd-text")
-
-    return doc
 
 
 if __name__ == "__main__":
