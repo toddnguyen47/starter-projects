fe68ac1775ea312a2fb0aed06fbd79eac742680c
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2021-11-15 15:15:22 -0800

    Update run_eclipse_java_format.py

--- java/pre-commit/pre_commit_hooks/run_eclipse_java_format.py
+++ java/pre-commit/pre_commit_hooks/run_eclipse_java_format.py
@@ -5,49 +5,46 @@ import sys
 import argparse
 import subprocess
 import os.path
-import collections
 
 _ECLIPSE_EXE = ""
 _FORMATTER_CONFIG = ""
 
+_CMD = [
+    _ECLIPSE_EXE,
+    "-nosplash",
+    "-data",
+    "/tmp/eclipse-workspace",
+    "-application",
+    "org.eclipse.jdt.core.JavaCodeFormatter",
+    "-config",
+    _FORMATTER_CONFIG,
+]
 
 def execute_formatter(list_of_files: List[str]) -> int:
     """Execute `Eclipse formatter` command. Note that this command needs Java 11+"""
-    cmd = collections.deque(
-        [
-            _ECLIPSE_EXE,
-            "-nosplash",
-            "-data",
-            "/tmp/eclipse-workspace",
-            "-application",
-            "org.eclipse.jdt.core.JavaCodeFormatter",
-            "-config",
-            _FORMATTER_CONFIG,
-        ]
-    )
     return_code = 0
 
+    list_of_abs_path_files = []
     for input_file in list_of_files:
-        abs_path = os.path.abspath(input_file)
-        cmd.append(abs_path)
+        if input_file.endswith(".java"):
+            abs_path = os.path.abspath(input_file)
+            list_of_abs_path_files.append(abs_path)
 
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
-        str_to_write = result.stdout.decode("utf-8").strip()
-        print(str_to_write)
-        return_code = max(return_code, result.returncode)
-
-        cmd.pop()
-
+    cmd = _CMD + list_of_abs_path_files
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
+    str_to_write = result.stdout.decode("utf-8").strip()
+    print(str_to_write)
+    return_code = max(return_code, result.returncode)
     return return_code
 
 
 def main(_argv: Optional[Sequence[str]] = None) -> int:
     """Main function to run our checks"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("files", nargs="*", help="list of files")
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
     args = parser.parse_args()
 
-    return execute_formatter(args.files)
+    return execute_formatter(args.filenames)
 
 
 if __name__ == "__main__":
