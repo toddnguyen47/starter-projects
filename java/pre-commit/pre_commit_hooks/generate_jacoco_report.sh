0e0977cc1634aecd39652c878f96239cecf8174f
Todd Nguyen <todd.nguyen47@gmail.com>
2021-12-15 17:58:23 -0800

    Whoops, wrong env var in `generate_jacoco_report`

--- java/pre-commit/pre_commit_hooks/generate_jacoco_report.sh
+++ java/pre-commit/pre_commit_hooks/generate_jacoco_report.sh
@@ -2,16 +2,16 @@
 
 set -euxo pipefail
 
-# Must set `_HTML_FILEPATH`` and `_PATH_TO_JACOCOCLI_JAR` as env variables
+# Must set `_JACOCO_HTML_FILEPATH`` and `_PATH_TO_JACOCOCLI_JAR` as env variables
 # using `source`
 # You can obtain the latest JAR here: https://github.com/jacoco/jacoco
 
-rm -rf "${_HTML_FILEPATH}"
+rm -rf "${_JACOCO_HTML_FILEPATH}"
 
 java \
     -jar "${_PATH_TO_JACOCOCLI_JAR}" \
     report target/jacoco.exec \
     --classfiles target/classes \
     --sourcefiles src/main/java \
-    --html "${_HTML_FILEPATH}"
+    --html "${_JACOCO_HTML_FILEPATH}"
 
